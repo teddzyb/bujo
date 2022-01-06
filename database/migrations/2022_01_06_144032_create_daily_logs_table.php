1b634a4cfbec030984e5a6939d169dc8d66526eb
Edwin Bartlett <teddybartletg88@gmail.com>
2022-01-06 22:49:33 +0800

    Added daily_logs model

---
+++ database/migrations/2022_01_06_144032_create_daily_logs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDailyLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('daily_logs', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('date');
+            $table->string('title');
+            $table->string('mood');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('daily_logs');
+    }
+}
