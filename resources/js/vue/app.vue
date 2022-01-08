247a212a1f1566ca0b5eb20bd6434527b34e8968
Edwin Bartlett <teddybartletg88@gmail.com>
2022-01-08 16:20:10 +0800

    Moved assets to public folder

--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,7 +1,69 @@
 <template>
-    <div>
-        Hello World!
-    </div>
+    <!-- Sidenav -->
+    <nav class="sidenav navbar navbar-vertical fixed-left navbar-expand-xs navbar-light bg-white" id="sidenav-main">
+        <div class="scrollbar-inner">
+            <!-- Brand -->
+            <div class="sidenav-header  align-items-center">
+                <a class="navbar-brand" href="#">
+                    <img src="assets/img/brand/blue.png" class="navbar-brand-img" alt="Bujo">
+                </a>
+            </div>
+            <div clagulpss="navbar-inner">
+                <!-- Collapse -->
+                <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+                    <!-- Nav items -->
+                    <ul class="navbar-nav px-4">
+                        <li class="nav-item">
+                            <a class="nav-link" href='#'>
+                                <i class="ni ni-chart-pie-35 text-blue"></i>
+                                <span class="nav-link-text">Dashboard</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href='#'>
+                                <i class="ni ni-book-bookmark text-orange"></i>
+                                <span class="nav-link-text">Daily Log</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href='#'>
+                                <i class="ni ni-satisfied text-green"></i>
+                                <span class="nav-link-text">Activity Tracker</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href='#'>
+                                <i class="ni ni-chart-bar-32 text-teal"></i>
+                                <span class="nav-link-text">Hydration Tracker</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href='#'>
+                                <i class="ni ni-favourite-28 text-red"></i>
+                                <span class="nav-link-text">Sleep Log</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <!-- Divider -->
+                    <hr class="my-3">
+                    <ul class="navbar-nav px-4">
+                        <li class="nav-item">
+                            <a class="nav-link" href='#'>
+                                <i class="ni ni-single-02 text-purple"></i>
+                                <span class="nav-link-text">Profile</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href='#'>
+                                <i class="ni ni-settings text-red"></i>
+                                <span class="nav-link-text">Settings</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,19 +2,36 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
+        <meta name="author" content="Creative Tim">
+        <title>Bujo</title>
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet"></link>
+        <!-- Icons -->
+        <link href="{{ asset('vendor/nucleo/css/nucleo.css') }}" rel="stylesheet"></link>
+        <link href="{{ asset('vendor/fortawesome/fontawesome-free/css/all.min.css') }}" rel="stylesheet"></link>
+        <!-- Argon CSS -->
+        <link href="{{ asset('css/argon.css?v=1.2.0') }}" rel="stylesheet"></link>
     </head>
     <body>
         <div id="app">
             <app></app>
+            <p>Mic test</p>
         </div>
 
+        <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}"></script>
+        <!-- Core -->
+        <script src="{{ asset('assets/vendor/jquery/dist/jquery.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/js-cookie/js.cookie.js') }}"></script>
+        <script src="{{ asset('assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js') }}"></script>
+        <!-- Optional JS -->
+        <script src="{{ asset('assets/vendor/chart.js/dist/Chart.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/chart.js/dist/Chart.extension.js') }}"></script>
+        <!-- Argon JS -->
+        <script src="{{ asset('assets/js/argon.js?v=1.2.0') }}"></script>
     </body>
 </html>
