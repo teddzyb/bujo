c355241a72fcccc4d3754038b09fcbe8c9f9b7f1
Edwin Bartlett <teddybartletg88@gmail.com>
2022-01-09 22:46:18 +0800

    Optimization

--- resources/js/components/Content.vue
+++ resources/js/components/Content.vue
@@ -246,19 +246,19 @@
             <div class="row align-items-center justify-content-lg-between">
                 <div class="col-lg-6">
                     <div class="copyright text-center  text-lg-left  text-muted">
-                        &copy; 2022 <a href="#" class="font-weight-bold ml-1 text-green" target="_blank">Bujo</a>
+                        &copy; 2022 <a href="#" class="font-weight-bold ml-1 text-green">Bujo</a>
                     </div>
                 </div>
                 <div class="col-lg-6">
                     <ul class="nav nav-footer justify-content-center justify-content-lg-end">
                         <li class="nav-item">
-                            <a href="#" class="nav-link" target="_blank">Bujo</a>
+                            <a href="#" class="nav-link">Bujo</a>
                         </li>
                         <li class="nav-item">
-                            <a href="#" class="nav-link" target="_blank">About Us</a>
+                            <a href="#" class="nav-link">About Us</a>
                         </li>
                         <li class="nav-item">
-                            <a href="#" class="nav-link" target="_blank">Blog</a>
+                            <a href="#" class="nav-link">Blog</a>
                         </li>
                         <li class="nav-item">
                             <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
