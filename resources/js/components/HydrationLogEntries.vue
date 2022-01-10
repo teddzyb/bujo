a42ff2ab588fd65888f328aeee9d9ece57c99e01
Edwin Bartlett <teddybartletg88@gmail.com>
2022-01-10 22:51:58 +0800

    Hydration Tracker fully functional

---
+++ resources/js/components/HydrationLogEntries.vue
@@ -0,0 +1,32 @@
+<template>
+    <table class="table align-items-center table-flush">
+        <thead class="thead-light">
+            <tr>
+                <th scope="col" class="sort" data-sort="date">Date</th>
+                <th scope="col" class="sort" data-sort="activity">Glasses</th>
+                <th scope="col" class="sort" data-sort="description">Description</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody class="list">
+            <HydrationLogEntry v-on:updateEntry="updateEntry" v-on:updateLogs="$emit('updateLogs')" v-for="(entry, index) in entries" :key="index" :entry="entry" />
+        </tbody>
+    </table>
+</template>
+
+<script>
+import HydrationLogEntry from './HydrationLogEntry.vue';
+
+export default {
+    name: "HydrationLogEntries",
+    props: ['entries'],
+    components: {
+        HydrationLogEntry
+    },
+    methods: {
+        updateEntry(id) {
+            this.$emit('updateEntry', id);
+        }
+    }
+}
+</script>
